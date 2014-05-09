3c79ee3115387bcf34c56a929f2e181de978a072
Martin Wozniewicz <martin.woz@gmail.com>
2014-05-08 21:34:30 -0500

    Undo changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Ensure we are on https
-var host = "mpwoz.github.io";
-if ((host == window.location.host) && (window.location.protocol != "https:"))
-    window.location.protocol = "https";
+//var host = "mpwoz.github.io";
+//if ((host == window.location.host) && (window.location.protocol != "https:"))
+    //window.location.protocol = "https";
 
 
 var recognition = new webkitSpeechRecognition();
